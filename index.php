8a4698645d1d017dd8308c30979fc269e5162ab1
Marcelo da Cunha <>
2017-12-15 12:33:07 -0200

    Alter Title - index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
    <head>
       <meta charset="UTF-8">
-      <title>GoldenView</title>
+      <title>GoldenView - Test01</title>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <link rel="shortcut icon" href="http://goldenview-novo/img/favicon.png"/>
       <link rel="stylesheet" href="http://goldenview-novo/css/bootstrap.min.css"> 
